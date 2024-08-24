7db5dd19abb263fd83adb6f24e42eb20c4f44e7e
NiLNight <bkmz-2020@inbox.ru>
2024-08-24 21:37:11 +0300

    добавлена функция регистрации нового пользователя

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,7 +2,25 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request,
+                          'account/template/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/template/register.html',
+                  {'user_form': user_form})
 
 
 def user_login(request):
@@ -28,6 +46,6 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-   return render(request,
-                 'account/dashboard.html',
-                 {'section': 'dashboard'})
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
