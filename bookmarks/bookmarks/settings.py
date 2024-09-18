e30019e0e30f3708369b48a24d0706add537fc6f
NiLNight <bkmz-2020@inbox.ru>
2024-09-18 17:15:59 +0300

    добавлена загрузка изображений

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'django_extensions'
+    'images.apps.ImagesConfig',
 ]
 
 MIDDLEWARE = [
